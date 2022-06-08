1c9ec7c5561dc5ed657e36fe33e66dfb5252f449
Adrian <adrian.tay@live.com>
2022-06-08 23:03:01 +0800

    Added: Project utils

---
+++ src/utils/Colors.js
@@ -0,0 +1,6 @@
+export default {
+  primary: '#5604CC',
+  white: '#FFFF',
+  mediumGrey: '#828282',
+  lightGrey: '#D3D3D3',
+};
---
+++ src/utils/Constants.js
@@ -0,0 +1,6 @@
+export const SEARCH_BY_MENU = {
+  ALL: 'All',
+  NAME: 'Name',
+  EMAIL: 'Email',
+  COMMENT: 'Comment',
+};
